da6624bf2b254eff255b4b1d442584b33a5171a2
Divyank <divyank16khajuria@gmail.com>
2023-05-03 17:05:19 +0530

    finally working but time complexity is not good

--- Leetcode/29.divide-two-integers.cpp
+++ Leetcode/29.divide-two-integers.cpp
@@ -8,37 +8,43 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
+        //Any way to continue this approach
+        //This approach giving error on when INT_MIN is negated which is out of bound
         if(dividend == INT_MIN && divisor == -1){
             return INT_MAX;
         }
         if(dividend == INT_MIN && divisor == 1){
             return INT_MIN;
         }
+        if(dividend == INT_MAX && divisor == 1){
+            return INT_MAX;
+        }
+        if(dividend == INT_MAX && divisor == -1){
+            return -INT_MAX;
+        }
+         unsigned int dd=abs(dividend);
+        unsigned int dv=abs(divisor);
         if((dividend>0 && divisor>0) || (dividend<0 && divisor<0)){
-        if(dividend<0)dividend=-dividend;
-        if(divisor<0)divisor=-divisor;
         int ans=0;
-        while(dividend>=0){
-           dividend-=divisor;
-           if(dividend>=0){
-           ans++;}
+        while(dd>=dv){
+           dd-=dv;
+           ans++;
         }
     return ans;
     }
-    else if((dividend==0)){
+    else if((dd==0)){
         return 0;
     }
     else{
-        if(dividend<0)dividend=-dividend;
-        if(divisor<0)divisor=-divisor;
          int ans=0;
-        while(dividend>=0){
-           dividend-=divisor;
-           if(dividend>=0){
-           ans++;}
+        while(dd>=dv){
+           dd-=dv;
+           ans++;
         }
     return -ans;
     }
+    //---------------Bits--Approach--------------------
+
     }
 };
 // @lc code=end
