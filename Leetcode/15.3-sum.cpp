f6f09e9bc9403f60abed8242ae4c083a788fb0c5
Divyank <divyank16khajuria@gmail.com>
2023-05-04 17:36:19 +0530

    more optimal log(n) extra time compl. reduced

--- Leetcode/15.3-sum.cpp
+++ Leetcode/15.3-sum.cpp
@@ -9,7 +9,6 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>>ans;
-        set<vector<int>>p;
         int n=nums.size();
         sort(nums.begin(),nums.end());
         for(int i=0;i<n-2;i++){
@@ -18,9 +17,15 @@ public:
             int target=-nums[i];
             while(l<r){
                 if(nums[l]+nums[r]==target){
-                 p.insert({nums[i],nums[l],nums[r]});
-                 l++;
-                 r--;
+                    vector<int>v;
+                    v.push_back(nums[i]);
+                    v.push_back(nums[l]);
+                    v.push_back(nums[r]);
+                 ans.push_back(v);
+                 int left=l;
+                 int right=r;
+                 while(l<r && nums[l]==nums[left])l++;
+                 while(l<r && nums[r]==nums[right])r--;
                 }
                 else if(nums[l]+nums[r]<target){
                     l++;
@@ -29,9 +34,7 @@ public:
                     r--;
                 }
             }
-        }
-        for(auto i:p){
-            ans.push_back(i);
+            while(i+1<n && nums[i]==nums[i+1])i++;
         }
         return ans;
     }
--- Leetcode/18.4-sum.cpp
+++ Leetcode/18.4-sum.cpp
@@ -8,7 +8,34 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<vector<int>>ans;
+        for(int i=0;i<n-3;i++){
+            for(int j=i+1;j<n-2;j++){
+                int l=j+1;
+                int r=n-1;
+                while(l<r){
+                    long long tg=(long long)target-((long long)nums[i]+(long long)nums[j]);
+                    if(nums[l]+nums[r]==tg){
+                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
+                        int left=l;
+                        int right=r;
+                        while(l<r && nums[left]==nums[l])l++;
+                        while(l<r && nums[right]==nums[r])r--;
+                    }
+                    else if(nums[l]+nums[r]<tg){
+                        l++;
+                    }
+                    else{
+                        r--;
+                    }
+                }
+                while(j+1<n && nums[j]==nums[j+1])j++;
+            }
+            while(i+1<n && nums[i]==nums[i+1])i++;
+        }
+        return ans;
     }
 };
 // @lc code=end
