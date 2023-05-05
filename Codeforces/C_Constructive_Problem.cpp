7e0f55495afc8f541abbda41fcda19f2e5095e11
Divyank <divyank16khajuria@gmail.com>
2023-05-05 19:10:50 +0530

    abhi krna hai isse

---
+++ Codeforces/C_Constructive_Problem.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define forn(i,n) for(ll i=0;i<n;i++)
+void solve(){
+ll n;
+cin>>n;
+ll a[n];
+forn(i,n)cin>>a[i];
+
+}
+int main(){
+ll t;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
