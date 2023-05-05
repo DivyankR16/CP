219079eedb022f8440d5f4c446cc126ad6058c7e
Divyank <divyank16khajuria@gmail.com>
2023-05-05 17:10:24 +0530

    Ian and sort the array

--- Codeforces/C_Ian_and_Array_Sorting.cpp
+++ Codeforces/C_Ian_and_Array_Sorting.cpp
@@ -3,7 +3,25 @@ using namespace std;
 #define ll long long
 #define forn(i,n) for(ll i=0;i<n;i++)
 void solve(){
-
+ll n;
+cin>>n;
+ll a[n];
+forn(i,n)cin>>a[i];
+if(n%2==1){
+    cout<<"YES"<<"\n";
+    return;
+}
+for(ll i=1;i<n-1;i++){
+    ll diff=a[i-1]-a[i];
+    a[i]+=diff;
+    a[i+1]+=diff;
+}
+if(a[n-2]>a[n-1]){
+    cout<<"NO"<<"\n";
+}
+else{
+    cout<<"YES"<<"\n";
+}
 }
 int main(){
 ll t;
