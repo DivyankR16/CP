3478259718d20758934a12f540f067a678b8de93
Divyank <divyank16khajuria@gmail.com>
2023-05-05 21:37:15 +0530

    spelling mistake corrected

--- Codeforces/C_Constructive_Problem.cpp
+++ Codeforces/C_Constructive_Problem.cpp
@@ -48,7 +48,7 @@ if(startidx!=INT_MIN){
 // And check if all elements in that segement greater than mex of array if they are then answer is yes otherwise no
 // But if mex+1 is only appered once then,replace it with mex and answer is yes
 //But if mex+1 is not present in the array and there is any element which is greater than mex of the array then replace it with mex and answer is yes
-// else if mext_1 is not present in the array and there is no element which is gretaer than mex of array then it is not [ossible to replace so answer is no
+// else if mext_1 is not present in the array and there is no element which is gretaer than mex of array then it is not possible to replace so answer is no
 }
 int main(){
 ll t;
