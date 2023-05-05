5c66dabd5fc5eecb8e216279824c932b30892883
Divyank <divyank16khajuria@gmail.com>
2023-05-05 21:34:22 +0530

    refined commented code

--- Codeforces/C_Constructive_Problem.cpp
+++ Codeforces/C_Constructive_Problem.cpp
@@ -46,7 +46,9 @@ if(startidx!=INT_MIN){
 //Approach is simple but code is lengthy
 // I will search for a subsegment which start with firstmost mex+1 of array and end with endmost mex+1 of array 
 // And check if all elements in that segement greater than mex of array if they are then answer is yes otherwise no
-// But if mex+1 is only appered once then,
+// But if mex+1 is only appered once then,replace it with mex and answer is yes
+//But if mex+1 is not present in the array and there is any element which is greater than mex of the array then replace it with mex and answer is yes
+// else if mext_1 is not present in the array and there is no element which is gretaer than mex of array then it is not [ossible to replace so answer is no
 }
 int main(){
 ll t;
