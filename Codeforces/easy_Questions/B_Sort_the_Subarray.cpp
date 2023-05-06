89f995f7ebbe39ccbd283a1e94a8eda255ed0af7
Divyank <divyank16khajuria@gmail.com>
2023-05-06 14:45:26 +0530

    half done

--- Codeforces/easy_Questions/B_Sort_the_Subarray.cpp
+++ Codeforces/easy_Questions/B_Sort_the_Subarray.cpp
@@ -3,6 +3,16 @@ using namespace std;
 #define ll long long
 #define forn(i,n) for(ll i=0;i<n;i++)
 void solve(){
+ll n;
+cin>>n;
+ll a[n];
+forn(i,n)cin>>a[i];
+ll b[n];
+forn(i,n)cin>>b[i];
+if(a[0]!=b[0] && a[n-1]!=b[n-1]){
+    cout<<1<<" "<<n<<"\n";
+    return;
+}
 
 }
 int main(){
