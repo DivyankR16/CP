99f6a407d84a27d3db9a78894d20aebd7624a8b9
Divyank <divyank16khajuria@gmail.com>
2023-05-07 12:12:40 +0530

    one more question

--- Codeforces/easy_Questions/B_Sort_the_Subarray.cpp
+++ Codeforces/easy_Questions/B_Sort_the_Subarray.cpp
@@ -13,7 +13,36 @@ if(a[0]!=b[0] && a[n-1]!=b[n-1]){
     cout<<1<<" "<<n<<"\n";
     return;
 }
-
+ll check=true;
+forn(i,n-1){
+    if(a[i]>a[i+1]){
+        check=false;
+        break;
+    }
+}
+if(check){
+    cout<<1<<" "<<n-1<<"\n";
+    return;
+}
+ll startidx;
+ll endidx;
+forn(i,n){
+if(a[i]!=b[i]){
+    startidx=i;
+    // cout<<startidx<<"\n";
+    break;
+}
+}
+forn(i,n){
+    if(a[n-i-1]!=b[n-i-1]){
+        endidx=n-i-1;
+        break;
+    }
+}
+// cout<<startidx<<" "<<endidx<<"\n";
+while(startidx>=1 && b[startidx-1]<=b[startidx])startidx--;
+while(endidx<=n-2 && b[endidx]<=b[endidx+1])endidx++;
+cout<<startidx+1<<" "<<endidx+1<<"\n";
 }
 int main(){
 ll t;
