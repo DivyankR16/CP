bc9ebd737c22371666fb400488cbdf9634112f36
Divyank <divyank16khajuria@gmail.com>
2023-05-08 15:54:25 +0530

    done

--- Codeforces/C_Tear_It_Apart.cpp
+++ Codeforces/C_Tear_It_Apart.cpp
@@ -5,7 +5,25 @@ using namespace std;
 void solve(){
 string s;
 cin>>s;
-
+ll n=s.length();
+ll ans=LONG_LONG_MAX;
+for(char c='a';c<='z';c++){
+    ll maxCntOf0=0;
+    ll i=0;
+    while(i<n){
+        ll cnt=0;
+        while(i<n && s[i]!=c){
+            cnt++;
+            i++;
+        }
+        maxCntOf0=max(maxCntOf0,cnt);
+        i++;
+    }
+   ll cnter=0;
+   while((1<<cnter)<=maxCntOf0)cnter++;
+   ans=min(ans,cnter);
+}
+cout<<ans<<"\n";
 }
 int main(){
 ll t;
