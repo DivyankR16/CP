ee9ce65f6bb119a044f96f49c251faa28d267d54
Divyank <divyank16khajuria@gmail.com>
2023-05-09 18:04:15 +0530

    normal cgange

--- Codeforces/Way_to+Specialist/D_Same_Differences.cpp
+++ Codeforces/Way_to+Specialist/D_Same_Differences.cpp
@@ -7,19 +7,7 @@ ll n;
 cin>>n;
 ll a[n];
 forn(i,n)cin>>a[i];
-vector<pair<ll,ll>>ans;
-forn(i,n){
-   ans.push_back({a[i],i+1});
-}
-sort(ans.begin(),ans.end());
-ll cnt=0;
-for(ll j=n-1;j>=0;j--){
-    for(ll i=n-1;i>=0;i--){
-        if(i!=j && ans[j].first-ans[i].first<1)break;
-        if(ans[j].second>ans[i].second && ans[j].second-ans[i].second==ans[j].first-ans[i].first)cnt++;
-    }
-}
-cout<<cnt<<"\n";
+//
 }
 int main(){
 ll t;
