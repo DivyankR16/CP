4492c70513c57025c2f83d90851d692a5404ac52
Divyank <divyank16khajuria@gmail.com>
2023-05-09 18:08:34 +0530

    corrected

--- Codeforces/Way_to+Specialist/D_Same_Differences.cpp
+++ Codeforces/Way_to+Specialist/D_Same_Differences.cpp
@@ -7,7 +7,16 @@ ll n;
 cin>>n;
 ll a[n];
 forn(i,n)cin>>a[i];
-//
+//aj-j=ai-i
+unordered_map<ll,ll>m;
+forn(i,n){
+    m[a[i]-i]++;
+}
+ll ans=0;
+for(auto i:m){
+  ans+=(i.second*(i.second-1)/2);
+}
+cout<<ans<<"\n";
 }
 int main(){
 ll t;
