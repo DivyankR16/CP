d697477806a92e7b102c63f4f93a8c230b3ec6c4
Divyank <divyank16khajuria@gmail.com>
2023-05-13 20:41:01 +0530

    more changes

---
+++ Codeforces/Way_to+Specialist/B_I_Hate_1111.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define forn(i,n) for(ll i=0;i<n;i++)
+void solve(){
+ll x;
+cin>>x;
+if(x)
+}
+int main(){
+ll t;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
--- Codeforces/Way_to+Specialist/B_Nastia_and_a_Good_Array.cpp
+++ Codeforces/Way_to+Specialist/B_Nastia_and_a_Good_Array.cpp
@@ -7,6 +7,20 @@ ll n;
 cin>>n;
 ll a[n];
 forn(i,n)cin>>a[i];
+ll minVal=INT_MAX;
+ll minPos=-1;
+forn(i,n){
+    if(a[i]<minVal){
+        minVal=a[i];
+        minPos=i;
+    }
+}
+cout<<n-1<<"\n";
+forn(i,n){
+    if(i!=minPos){
+        cout<<minPos+1<<" "<<i+1<<" "<<minVal<<" "<<minVal+abs(minPos-i)<<"\n";
+    }
+}
 }
 int main(){
 ll t;
