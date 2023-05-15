8fa9da39bc95e8c5a9ad876cff67e64bd09ef958
Divyank <divyank16khajuria@gmail.com>
2023-05-15 14:29:59 +0530

    done

--- Codeforces/Contests/33/C_Counting_Orders.cpp
+++ Codeforces/Contests/33/C_Counting_Orders.cpp
@@ -5,11 +5,21 @@ using namespace std;
 void solve(){
 ll n;
 cin>>n;
+ll need=1e9+7;
 ll a[n];
 forn(i,n)cin>>a[i];
 ll b[n];
 forn(i,n)cin>>b[i];
-
+sort(a,a+n);
+sort(b,b+n);
+ll cnter=0;
+ll ans=1;
+for(ll i=n-1;i>=0;i--){
+   ll val=n-(upper_bound(a,a+n,b[i])-a)-cnter;
+   ans=(ans*val)%need;
+   cnter++;
+}
+cout<<ans<<"\n";
 }
 int main(){
 ll t;
