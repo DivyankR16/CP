14a5eb96bc3cea485fc8d6fefe2a44d7c3c298fa
Divyank <divyank16khajuria@gmail.com>
2023-05-25 19:49:08 +0530

    ok

---
+++ Codeforces/Contests/34/A_Watermelon.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define forn(i,n) for(ll i=0;i<n;i++)
+void solve(){
+    ll n;
+    cin>>n;
+if(n==2 || n%2==1){
+  cout<<"NO"<<endl;
+}
+else{cout<<"YES"<<endl;}
+}
+int main(){
+solve();
+ 
+return 0;
+}
--- Codeforces/Way_to+Specialist/B_Box_Fitting.cpp
+++ Codeforces/Way_to+Specialist/B_Box_Fitting.cpp
@@ -3,7 +3,31 @@ using namespace std;
 #define ll long long
 #define forn(i,n) for(ll i=0;i<n;i++)
 void solve(){
+ll n,W;
+cin>>n>>W;
+ll w[n];
+forn(i,n)cin>>w[i];
+sort(w,w+n);
+ll i=0;
+ll j=n-1;
 
+ll height=0;
+while(i<=j){
+    ll val;
+    if(i!=j){
+    val=w[i];}
+    else{
+        val=0;
+    }
+  while(i<=j && w[j]+val<=W){
+     i++;
+     if(i<=j){
+     val+=w[i];}
+  }
+  height++;
+  j--;
+}
+cout<<height<<"\n";
 }
 int main(){
 ll t;
