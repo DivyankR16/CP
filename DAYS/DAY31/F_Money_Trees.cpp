858d48e07de577ca7e2759a314fc360518c30c76
Divyank <divyank16khajuria@gmail.com>
2023-10-03 22:38:01 +0530

    easy question but still happy (weak topic)

--- DAYS/DAY31/F_Money_Trees.cpp
+++ DAYS/DAY31/F_Money_Trees.cpp
@@ -2,34 +2,36 @@
 using namespace std;
 #define ll long long
 #define forn(i,n) for(ll i=0;i<n;i++)
+ll findMaxLen(ll a[],ll st,ll end,ll k){
+    ll ans=0;
+    ll sum=0;
+    ll indst=st;
+    for(ll indend=st;indend<=end;indend++){
+       sum+=a[indend];
+       while(indst<=indend && sum>k){
+        sum-=a[indst];
+        indst++;
+       }
+       ans=max(ans,indend-indst+1);
+    }
+    return ans;
+}
 void solve(){
 ll n,k;
 cin>>n>>k;
 ll a[n],h[n];
 forn(i,n)cin>>a[i];
 forn(i,n)cin>>h[i];
-ll i=0;
+ll st=0;
+ll end=0;
 ll ans=0;
-while(i<n){
-    ll previdx=i;
-    ll cnt=1;
-    ll fruits=a[i];
-    if(fruits<=k){
-        ans=max(ans,(ll)1);
-    }
-    while(i+1<n && h[i]%h[i+1]==0){
-        fruits+=a[i+1];
-        i++;
-        cnt++;
-        if(fruits<=k){
-            ans=max(ans,cnt);
-        }
-        while(previdx<=i && fruits>k){
-               fruits-=a[previdx];
-               previdx++;
-        }
-    }
-    i++;
+while(end<n){
+while(end+1<n && h[end]%h[end+1]==0){
+    end++;
+}
+ans=max(ans,findMaxLen(a,st,end,k));
+end++;
+st=end;
 }
 cout<<ans<<"\n";
 }
