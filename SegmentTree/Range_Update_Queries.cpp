f4a74067bd01dcc1e7320362287114889ee976c0
Divyank <divyank16khajuria@gmail.com>
2023-10-05 02:03:28 +0530

    segemntTreeRangeUpdate(Choke)

--- SegmentTree/Range_Update_Queries.cpp
+++ SegmentTree/Range_Update_Queries.cpp
@@ -3,13 +3,15 @@ using namespace std;
 #define ll long long
 #define forn(i,n) for(ll i=0;i<n;i++)
 vector<ll>tree;
+vector<ll>lazy;
 ll build(vector<ll>&a){
     ll n=a.size();
-    while(__builtin_popcount(n)!=1){
+    while(__builtin_popcountll(n)!=1){
         a.push_back(0);
         n++;
     }
     tree.resize(2*n);
+    lazy.resize(2*n);
     forn(i,n){
         tree[n+i]=a[i];
     }
@@ -18,18 +20,47 @@ ll build(vector<ll>&a){
     }
     return n;
 }
-void update(ll node,ll segleft,ll segright,ll qleft,ll qright,ll u){
-    if(segleft==segright){
-      tree[node]+=u;
-      return;
-    }
-    if(qleft>segright || qright<segleft){
+void update(ll node,ll segleft,ll segright,ll qleft, ll qright,ll value){
+     if(lazy[node]){
+        tree[node]+=lazy[node]*(segright-segleft+1);
+        if(segleft!=segright){
+            lazy[2*node]+=lazy[node];
+            lazy[2*node+1]+=lazy[node];
+        }
+        lazy[node]=0;
+     }
+    if(segleft>qright || segright<qleft){
         return;
     }
+     if(segleft>=qleft && segright<=qright){
+        tree[node]=(segright-segleft+1)*value;
+        if(segleft!=segright){
+            lazy[2*node]+=value;
+            lazy[2*node+1]+=value;
+     }
+     }
+     ll lastidx=segleft+(segright-segleft)/2;
+     update(2*node,segleft,lastidx,qleft,qright,value);
+     update(2*node+1,lastidx+1,segright,qleft,qright,value);
+     tree[node]=tree[2*node]+tree[2*node+1];
+}
+ll query(ll node,ll segleft,ll segright,ll qleft,ll qright){
+    if(lazy[node]){
+        tree[node]+=lazy[node]*(segright-segleft+1);
+        if(segleft!=segright){
+            lazy[2*node]+=lazy[node];
+            lazy[2*node+1]+=lazy[node];
+        }
+        lazy[node]=0;
+    }
+    if(segleft>qright || segright<qleft){
+        return 0;
+    }
+    if(segleft>=qleft && segright<=qright){
+        return tree[node];
+    }
     ll lastidx=segleft+(segright-segleft)/2;
-    update(2*node,segleft,lastidx,qleft,qright,u);
-    update(2*node+1,lastidx+1,segright,qleft,qright,u);
-    tree[node]=tree[2*node]+tree[2*node+1];
+    return query(2*node,segleft,lastidx,qleft,qright)+query(2*node+1,lastidx+1,segright,qleft,qright);
 }
 void solve(){
 ll n,q;
@@ -50,12 +81,11 @@ else{
     ll k;
     cin>>k;
     k--;
-    cout<<tree[n+k]<<"\n";
+    cout<<query(1,0,n-1,k,k)<<"\n";
 }
 }
 }
 int main(){
-
 solve();
 return 0;
 }
