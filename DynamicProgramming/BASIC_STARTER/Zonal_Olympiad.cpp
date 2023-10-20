aec10b1cbfaf7771567df1f0d6da9600ed6340a1
Divyank <divyank16khajuria@gmail.com>
2023-10-20 11:26:37 +0530

    recursion+memo=Fast

--- DynamicProgramming/BASIC_STARTER/Zonal_Olympiad.cpp
+++ DynamicProgramming/BASIC_STARTER/Zonal_Olympiad.cpp
@@ -2,37 +2,55 @@
 using namespace std;
 #define ll long long
 #define forn(i, n) for (ll i = 0; i < n; i++)
+vector<ll> memo;
+ll ansRecur(ll a[], ll n, bool flag)
+{
+    if (n <= 2)
+    {
+        if (n == 0)
+        {
+            return a[0];
+        }
+        else if (n == 1)
+        {
+            if (flag)
+            {
+                return a[1];
+            }
+            else
+            {
+                return min(a[0], a[1]);
+            }
+        }
+        else
+        {
+            if (flag)
+            {
+                return a[2];
+            }
+            else
+            {
+                return min(min(a[0], a[1]), a[2]);
+            }
+        }
+    }
+    if (memo[n] != -1)
+    {
+        return memo[n];
+    }
+    return memo[n] = a[n] + min({ansRecur(a, n - 1, true), ansRecur(a, n - 2, true), ansRecur(a, n - 3, true)});
+}
 void solve()
 {
     ll n;
     cin >> n;
     ll a[n];
     forn(i, n) cin >> a[i];
-    if (n <= 3)
+    forn(i, n)
     {
-        if (n == 1)
-        {
-            cout << a[0] << "\n";
-        }
-        else if (n == 2)
-        {
-            cout << min(a[0], a[1]) << "\n";
-        }
-        else
-        {
-            cout << min(min(a[0], a[1]), a[2]) << "\n";
-        }
+        memo.push_back(-1);
     }
-    ll dp[n];
-    memset(dp, 0, sizeof(dp));
-    dp[0] = a[0];
-    dp[1] = a[1];
-    dp[2] = a[2];
-    for (int i = 3; i < n; i++)
-    {
-        dp[i] += a[i] + min(min(dp[i - 1], dp[i - 2]), dp[i - 3]);
-    }
-    cout << min(min(dp[n - 1], dp[n - 2]), dp[n - 3]) << "\n";
+    cout << min({ansRecur(a, n - 1, false), ansRecur(a, n - 2, false), ansRecur(a, n - 3, false)}) << "\n";
 }
 int main()
 {
