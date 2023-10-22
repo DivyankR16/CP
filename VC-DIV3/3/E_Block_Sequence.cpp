c580da812e1194f4f5e153274eb414ce62a258bf
Divyank <divyank16khajuria@gmail.com>
2023-10-23 00:16:53 +0530

    easy dp

--- VC-DIV3/3/E_Block_Sequence.cpp
+++ VC-DIV3/3/E_Block_Sequence.cpp
@@ -4,15 +4,28 @@ using namespace std;
 #define forn(i,n) for(ll i=0;i<n;i++)
 const ll maxx = 3e5;
 ll a[maxx];
+vector<ll> memo;
 ll recur(ll indx,ll n){
-    if(i==n-1){
-
+    if(indx==n){
+        return 0;
     }
+    if(memo[indx]!=-1)
+        return memo[indx];
+    ll ans = LONG_LONG_MAX;
+    if(indx+a[indx]+1<=n){
+        ans = min(ans, recur(indx + a[indx] + 1, n));
+    }
+    ans =min(ans,1+ recur(indx + 1, n));
+    // cout << "Just Checking:-> " << ans << "\n";
+    return memo[indx]=ans;
 }
 void solve(){
     ll n;
     cin >> n;
-    forn(i, n){
+    memo.clear();
+    memo.resize(n, -1);
+    forn(i, n)
+    {
         ll x;
         cin >> x;
         a[i] = x;
