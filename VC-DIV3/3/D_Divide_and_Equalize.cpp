f8081f17469a9ac6a456db565fe89e998627a831
Divyank <divyank16khajuria@gmail.com>
2023-10-22 12:32:32 +0530

    d

--- VC-DIV3/3/D_Divide_and_Equalize.cpp
+++ VC-DIV3/3/D_Divide_and_Equalize.cpp
@@ -7,7 +7,26 @@ void solve(){
     cin >> n;
     ll a[n];
     forn(i, n) cin >> a[i];
-    
+    map<ll, ll> mp;
+    forn(i,n){
+        ll x = a[i];
+        for (ll i = 2; i*i <= x;i++){
+            while(x%i==0){
+                mp[i]++;
+                x /= i;
+            }  
+    }
+    if(x>1)
+            mp[x]++;
+}
+for(auto i:mp){
+    if(i.second%n!=0){
+            cout << "NO"
+                 << "\n";
+            return;
+    }
+}
+cout<<"YES"<<'\n';
 }
 int main(){
 ll t;
