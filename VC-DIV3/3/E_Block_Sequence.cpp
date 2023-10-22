fb2ee734fa2cc0c551a97e138245775ca66adaf6
Divyank <divyank16khajuria@gmail.com>
2023-10-22 23:31:43 +0530

    block_Sequence

---
+++ VC-DIV3/3/E_Block_Sequence.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define forn(i,n) for(ll i=0;i<n;i++)
+const ll maxx = 3e5;
+ll a[maxx];
+ll recur(ll indx,ll n){
+    if(i==n-1){
+
+    }
+}
+void solve(){
+    ll n;
+    cin >> n;
+    forn(i, n){
+        ll x;
+        cin >> x;
+        a[i] = x;
+    }
+    cout<<recur(0, n)<<"\n";
+}
+int main(){
+ll t;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
