3a7a02fedf4773515ccd616d20785cf4540a0d56
Divyank <divyank16khajuria@gmail.com>
2023-10-25 01:28:34 +0530

    diificult to understand

---
+++ DynamicProgramming/Assignment1/A_Robbers_watch.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define forn(i,n) for(ll i=0;i<n;i++)
+void solve(){
+    ll n, m;
+    cin >> n >> m;
+    
+}
+int main(){
+solve();
+return 0;
+}
