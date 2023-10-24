1e648fe032113041e3fb758eee2740890f0207ef
Divyank <divyank16khajuria@gmail.com>
2023-10-24 23:23:16 +0530

    animals

--- DynamicProgramming/Assignment1/D_Animals.cpp
+++ DynamicProgramming/Assignment1/D_Animals.cpp
@@ -2,23 +2,36 @@
 using namespace std;
 #define ll long long
 #define forn(i,n) for(ll i=0;i<n;i++)
+// ll recur(ll n,ll a[],ll curidx,ll consumed,ll x){
+//     if(consumed==x || curidx==n){
+//         return 0;
+//     }
+//     ll ans = 0;
+//     ans = max(1+recur(n, a, curidx + 1, consumed + (ans + 1) * a[curidx], x),recur(n,a,curidx+1,consumed+(ans)*a[curidx],x));
+//     return ans;
+// }
 void solve(){
     ll n, x;
     cin >> n >> x;
     ll a[n];
     forn(i, n) cin >> a[i];
-    ll sum = 0;
+    ll dp[n];
+    forn(i,n){
+        dp[i] = (n - i) * a[i];
+    }
+    sort(dp, dp + n);
     ll ans = 0;
-    for (ll i = n - 1; i >= 0;i--){
-        sum += (ans + 1) * a[i];
-        if(sum<=x){
-            ans++;
-        }
-        else{
-            break;
-        }
+    forn(i,n){
+       if(x-dp[i]>=0){
+           ans++;
+           x -= dp[i];
+       }
+       else{
+           break;
+       }
     }
     cout << ans << "\n";
+    // cout << recur(n, a, 0, 0, x);
 }
 int main(){
     freopen("input.txt", "r", stdin);
