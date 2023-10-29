5d749bc68adba635fe00bec9faef0c40f363176e
Divyank <divyank16khajuria@gmail.com>
2023-10-29 14:08:58 +0530

    segment compression

---
+++ DAYS/DAY35/D_Constructing_the_Array.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define forn(i, n) for (ll i = 0; i < n; i++)
+void solve()
+{
+    ll n;
+    cin >> n;
+    set<pair<ll, ll>> s;
+    vector<ll> ans(n);
+    s.insert({-n, 0});
+    ll k = 1;
+    while (!s.empty())
+    {
+        auto cur = *(s.begin());
+        s.erase(s.begin());
+        ll len = -cur.first;
+        ll stidx = cur.second;
+        ll endidx = len + stidx - 1;
+        ll mid = (stidx + endidx) / 2;
+        ans[mid] = k++;
+        if (stidx < mid)
+            s.insert({-(mid - stidx), stidx});
+        if (endidx > mid)
+            s.insert({-(endidx - mid), mid + 1});
+    }
+    for (auto i : ans)
+    {
+        cout << i << " ";
+    }
+    cout << "\n";
+}
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ DAYS/DAY36/C_Medium_Design.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define forn(i,n) for(ll i=0;i<n;i++)
+ll costExculdingELE(vector<pair<ll,ll>>&segments,ll ele,ll n){
+    vector<ll> presum(n + 1, 0);
+    for(auto i:segments){
+        if(ele>=i.first && ele<=i.second){
+            continue;
+        }
+        presum[i.first]++;
+        presum[i.second+1]--;
+    }
+    ll ans = presum[0];
+    for (ll i = 1; i < n;i++){
+        presum[i] += presum[i - 1];
+        ans = max(ans, presum[i]);
+    }
+    return ans;
+}
+void solve(){
+    ll n, m;
+    cin >> n >> m;
+    vector<pair<ll, ll>> segments;
+    set<ll> s;
+    s.insert(0);
+    s.insert(m-1);
+    forn(i,n){
+        ll l, r;
+        cin >> l >> r;
+        l--;
+        r--;
+        segments.push_back({l, r});
+        s.insert(l);
+        s.insert(r);
+    }
+    map<ll, ll> mp;
+    ll pt = 0;
+    for(auto i:s){
+        mp[i] = pt++;
+    }
+    for (ll i = 0; i < n;i++){
+        segments[i].first = mp[segments[i].first];
+        segments[i].second = mp[segments[i].second];
+    }
+    // ll cnt = 1;
+    // for(auto i:segments){
+    //     cout <<"Compressed Segment "<<cnt<<"-> "<< i.first << " " << i.second << "\n";
+    //     cnt++;
+    // }
+    cout << max(costExculdingELE(segments, 0, pt), costExculdingELE(segments, pt - 1, pt)) << "\n";
+}
+int main(){
+ll t;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
