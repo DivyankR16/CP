c3b4858e911f22a25ed18e15af059562a7354d54
Divyank <divyank16khajuria@gmail.com>
2023-11-11 20:25:19 +0530

    ho gya question

--- VC-DIV2/13/C_Tea_Tasting.cpp
+++ VC-DIV2/13/C_Tea_Tasting.cpp
@@ -12,12 +12,23 @@ void solve(){
     presum[0] = b[0];
     for (ll i = 1;i < n; i++)
     {
-        presum[i]+= presum[i - 1] + b[i];
+        presum[i] = presum[i - 1] + b[i];
     }
+    // cout << lower_bound(presum, presum + n, 29) - presum << "\n";
+    // forn(i,n){
+    //     cout << presum[i] << " ";
+    // }
+    // cout << "\n";
     vector<ll> cnt(n, 0);
     vector<ll> extra(n, 0);
     for (ll i = 0; i < n;i++){
-        ll idx = lower_bound(presum, presum + n,a[i]+(i?presum[i-1]:0))-presum;
+        bool ck = true;
+        // cout << a[i] << "\n";
+        ll idx = lower_bound(presum+i, presum + n, a[i] + (i ? presum[i - 1] : 0)) - presum;
+        if(idx>=n){
+            ck = false;
+        }
+        idx = min(idx, n - 1);
         if (presum[idx] == a[i] + (i ? presum[i - 1] : 0)){
             cnt[i]+=1;
             if(idx+1<n){
@@ -25,11 +36,23 @@ void solve(){
             }
         }
         else{
+            if(ck){
             cnt[i] += 1;
             cnt[idx] -= 1;
-            extra[idx] += presum[idx] - a[i] - (i ? presum[i - 1] : 0);
+            extra[idx] += a[i]-(idx?presum[idx-1]-(i?presum[i-1]:0):0);
+            // cout << extra[idx] << "\n";
+            }
+            else{
+            cnt[i] += 1;
+            }
         }
     }
+    // cout << extra[n - 1] << "\n";
+    // cout << "\n";
+    // // forn(i,n){
+    // //     cout << extra[i] << " ";
+    // // }
+    // // cout << "\n";
     for (ll i = 0; i < n;i++){
         if(i)
             cnt[i] += cnt[i - 1];
---
+++ VC-DIV2/13/D_Triangle_Coloring.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define forn(i,n) for(ll i=0;i<n;i++)
+void solve(){
+
+}
+int main(){
+solve();
+return 0;
+}
