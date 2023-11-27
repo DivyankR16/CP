c601046fdf65f7a6b6357ea15a4dbfd7900bcc73
Divyank <divyank16khajuria@gmail.com>
2023-11-27 11:04:52 +0530

    hogya

--- Codeforces/Contests/67/C_Add_Divide_and_Floor.cpp
+++ Codeforces/Contests/67/C_Add_Divide_and_Floor.cpp
@@ -7,7 +7,31 @@ void solve(){
     cin >> n;
     ll a[n];
     forn(i, n) cin >> a[i];
-    
+    ll mn = a[0];
+    ll mx = a[0];
+    forn(i,n){
+        mn = min(mn, a[i]);
+        mx = max(mx, a[i]);
+    }
+    vector<ll> ans;
+    while(mn!=mx){
+        if(mn%2==1 && mx%2==0){
+            ans.push_back(1);
+            mn += 1;
+            mx += 1;
+        }
+        else{
+            ans.push_back(0);
+        }
+        mn /= 2;
+        mx /= 2;
+    }
+    cout << ans.size() << "\n";
+    if(ans.size()>n)return;
+    for(auto i:ans){
+        cout << i << " ";
+    }
+    cout << "\n";
 }
 int main(){
 ll t;
