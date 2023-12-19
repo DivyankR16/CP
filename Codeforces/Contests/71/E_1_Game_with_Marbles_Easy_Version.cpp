cfe2e27685ab31ec84b702faef10d5c8cccc60af
Divyank <divyank16khajuria@gmail.com>
2023-12-20 00:41:31 +0530

    div3 E1 E2 done

--- Codeforces/Contests/71/E_1_Game_with_Marbles_Easy_Version.cpp
+++ Codeforces/Contests/71/E_1_Game_with_Marbles_Easy_Version.cpp
@@ -8,7 +8,22 @@ void solve(){
     ll a[n], b[n];
     forn(i, n) cin >> a[i];
     forn(i, n) cin >> b[i];
-    
+    vector<pair<ll, ll>> v;
+    forn(i,n){
+        v.push_back({a[i], b[i]});
+    }
+    sort(v.begin(), v.end(), [&](const pair<ll, ll> &x, const pair<ll, ll> &y) -> bool
+         { return x.first + x.second > y.first + y.second; });
+    ll ans = 0;
+    forn(i,n){
+        if(i%2==0){
+            ans += v[i].first - 1;
+        }
+        else{
+            ans -= v[i].second - 1;
+        }
+    }
+    cout << ans << "\n";
 }
 int main(){
 ll t;
